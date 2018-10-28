57accea2160d3310d362cddfb15d2331ca529899
marsalan95 <42220477+marsalan95@users.noreply.github.com>
2018-10-28 11:33:50 +0500

    my assignment of circle

---
+++ circle.cpp
@@ -0,0 +1,27 @@
+#include<iostream.h>
+class Circle
+{
+private:
+Circle(): radius (4) {}
+void getradius ()
+{
+cout<<"Enter Radius Value;
+cin>>radius;
+}
+voud show radius()
+{
+cout<<"Radius is :"<<radius<<endl;
+}
+flaot area()
+{
+return 3.14*radius*radius;
+}
+};
+int maind()
+clrscr();
+Circle c1;
+c1.showradius ();
+cl.getRadius ();
+coutc1.aread();
+return 0;
+}
